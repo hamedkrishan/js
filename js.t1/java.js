ffe653320aab3166d70b40c8ab73465d004104f4
hamedkrishan <hamedkrishan676@gmail.com>
2024-12-30 11:05:29 +0300

    Add files via upload

---
+++ js.t1/java.js
@@ -0,0 +1,33 @@
+onload = function() {
+    document.write(typeof(100) + "<br>");
+    document.write(typeof(73.9) + "<br>");
+    document.write(typeof(NaN) + "<br>");
+    document.write(typeof("Water") + "<br>");
+    document.write(typeof(false) + "<br>");
+    document.write(typeof(9 != 11) + "<br>");
+    document.write("Orang" + "e"  + "<br>");
+    document.write("Orange" - "s"  + "<br>");
+    document.write("4" + "8"   + "<br>");
+    document.write("4" - "8"  + "<br>");
+    document.write("name" + 3  + "<br>");
+    document.write("name" - 3   + "<br>");
+    document.write(82 * "word"  + "<br>");
+    document.write(1 + "hello"  + "<br>");
+    document.write("hello" + 1   + "<br>");
+    document.write(1 + true   + "<br>");
+    document.write("hello" + true    + "<br>");
+    document.write(typeof (Infinity)    + "<br>");
+    document.write(1 == '1'   + "<br>");
+    document.write(1 === '1');
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
---
+++ js.t1/java1.js
@@ -0,0 +1,26 @@
+onload=function(){
+    var a , b , c , d , e , f , g , h , i , j , r , l , m , z;  
+ a = 1000;
+ b = 500;
+ c = 7;
+ d = 2;
+ e = 9;
+ f = 3;
+ g = 150;
+ h = .30;
+  i = 18;
+  j = 30;
+  r= 20;
+  l= i <r < j;
+ m = 10;
+ z = 4;
+ document.write(a/b + "<br>");
+ document.write(a-b + "<br>");
+ document.write(a+b + "<br>");
+ document.write(a*b + "<br>");
+ document.write((c+d+e)/f + "<br>"); 
+ document.write(g-g*h + "<br>"); 
+ document.write(l + "<br>");
+ document.write(d**f + "<br>");
+ document.write(m%z);
+ }
